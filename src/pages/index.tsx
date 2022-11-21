4a52db2427455f20d72d8b21d593b3e71450b44c
LucasSousa09 <lucasrodrigues.sousa09@gmail.com>
2022-11-21 19:28:40 -0300

    main session created

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,18 @@
+import Image from 'next/image'
+import mainImg from '../assets/farm_frances-gunn.jpg'
+
 import { Header } from "../components/Header";
 
 export default function Home() {
   return (
-    <>
+    <div className="bg-beige-100 max-w-[1440px] m-auto">
       <Header />
-    </>
+      <main className="relative pb-12">
+        <Image className='max-h-[679px] w-full object-cover' src={mainImg} alt="Celeiro Vermelho em uma área verde" />
+        <p className="absolute top-[50%] right-[50%] translate-x-[50%] translate-y-[-50%] text-[88px] leading-[110px] max-w-[714px] text-center text-brown-700 bg-beige-100/50 rounded-lg ">
+          Bem vindo à Fazenda Coração! 
+        </p>
+      </main>
+    </div>
   )
 }
